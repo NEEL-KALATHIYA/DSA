4ccef76f17c866d77eef0b2e98ffb1f9118ed7a4
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-02-01 22:52:34 +0530

    01/02/2024

---
+++ L-1/L-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+    int A = 4, B = 11; 
+  
+    cout << endl << "sum is = " << A+B; 
+    cout << endl << "sum is = " << A-B; 
+    cout << endl << "sum is = " << A*B; 
+
+    return 0; 
+} 
